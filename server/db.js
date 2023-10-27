b1e9adbe41e5a31921dda9974448e719bf9ba407
Michael Maquera <mike.maquera212@gmail.com>
2023-10-27 13:35:05 -0400

    Delete server/db.js

--- server/db.js
+++
@@ -1,7 +0,0 @@
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: "postgresql://postgres:1220105Maq!9bz@db.khqsigzoysrpnljdzvsi.supabase.co:5432/postgres",
-});
-
-module.exports = pool;
